7f0184b295275b9364b7d71bce4053502ae2d735
Plat <git@p1at.dev>
2023-04-11 16:45:36 +0900

    as a package

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='wd14tagger',
+    version='0.1',
+    description='Waifu Diffusion Tagger',
+    url='https://github.com/toriato/stable-diffusion-webui-wd14-tagger',
+    packages=['tagger'],
+)
